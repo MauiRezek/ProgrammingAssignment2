95897de6f23284164864b3c51a47a222cfb08d56
MauiRezek <46356886+MauiRezek@users.noreply.github.com>
2019-01-08 10:59:35 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+rm(list = ls())
 
-## Write a short comment describing this function
+## 2x2 square matrix
+mat <- matrix(c(1,2,3,4),nrow = 2,ncol = 2)
+
+## x <- makeCacheMatrix(mat) will initialize the special matrix "mat" (defined above)
+## x$get() returns whatever matrix was initialized and stored in x
+## x$set(matrix) will load a new matrix "matrix" into variable x 
+## x$getInverse() will return the inverse of the matrix stored in x. 
+## NOTE: If inverse has not been calcuated yet x$getInverse() will return NULL
+## x$setInverse(mat) will calculate the inverse of the matrix "mat" 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y = matrix()){
+    x <<- y
+    m <<- NULL
+  }
+  # calculates the inverse of a matrix
+  get <- function() x
+  setInverse <- function(solve) m <<- solve(x)
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(x) will return the inverse of matrix stored in "x" where "x" is previousy defined.
+## If x$setInverse(mat) was called earlier then cacheSolve(x) will return cached data.
+## If x$setInverse(mat) was not called then cacheSolve(x) will calculate and return the inverse 
+## of the matrix stored in x.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
